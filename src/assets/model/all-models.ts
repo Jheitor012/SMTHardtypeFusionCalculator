7d184927fc2fb8bf45c5926b4477ed68248915a6
Jose Heitor Pagotto <jhpagotto@hotmail.com>
2021-01-23 15:35:02 -0300

    added name and race to demons

--- src/assets/model/all-models.ts
+++ src/assets/model/all-models.ts
@@ -2,36 +2,36 @@ export class DemonsModel {
   race: string;
   lvl: number;
   name: string;
-  baseHP: number;
-  baseMP: number;
-  baseSt: number;
-  baseMa: number;
-  baseVi: number;
-  baseAg: number;
-  baseLu: number;
-  isBoss: boolean;
-  isBroker: boolean;
-  isEvolved: boolean;
-  resistances: ResistancesModel;
-  skills: SkillModel[];
+  // baseHP: number;
+  // baseMP: number;
+  // baseSt: number;
+  // baseMa: number;
+  // baseVi: number;
+  // baseAg: number;
+  // baseLu: number;
+  // isBoss: boolean;
+  // isBroker: boolean;
+  // isEvolved: boolean;
+  // resistances: ResistancesModel;
+  // skills: SkillModel[];
 }
 
-export class ResistancesModel {
-  phys: string;
-  fire: string;
-  ice: string;
-  elec: string;
-  force: string;
-  curse: string;
-  mind: string;
-  nerve: string;
-  expel: string;
-  death: string;
-}
+// export class ResistancesModel {
+//   phys: string;
+//   fire: string;
+//   ice: string;
+//   elec: string;
+//   force: string;
+//   curse: string;
+//   mind: string;
+//   nerve: string;
+//   expel: string;
+//   death: string;
+// }
 
-export class SkillModel {
-  id: number;
-  type: string;
-  name: string;
-  desc: string;
-}
+// export class SkillModel {
+//   id: number;
+//   type: string;
+//   name: string;
+//   desc: string;
+// }
